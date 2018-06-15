6b9bb6a48f5926b015677728df6c24a0360c37d7
ranying <ranying2013@163.com>
2018-06-15 18:16:31 +0800

    init

---
+++ feign/src/main/java/com/ranying/feign/FeignApplication.java
@@ -0,0 +1,21 @@
+package com.ranying.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
+public class FeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignApplication.class, args);
+    }
+
+}
---
+++ serviceribbon/src/main/java/com/ranying/ribbon/serviceribbon/ServiceRibbonApplication.java
@@ -0,0 +1,31 @@
+package com.ranying.ribbon.serviceribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 当SpringBoot的版本为  2.0 的时候，会出现各种报错
+ * 后来换成了 1.X 没有问题了
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
+public class ServiceRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRibbonApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+
+}
